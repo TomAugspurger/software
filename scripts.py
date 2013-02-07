fd48e17deab26ee4e1c801a5a8bcc67ca6cdc243
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-07 16:20:01 -0600

    Thinned out the labels a bit.

--- scripts.py
+++ scripts.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Change path to local version.
 s = pd.HDFStore('/Volumes/HDD/Users/tom/DataStorage/Patents/patents.h5')
@@ -7,4 +8,12 @@ df = s['utility']
 
 ## Counts by year
 gr = df.groupby(df['appyear'])
-fig = gr['patent'].count().plot(kind='bar')
+fig = gr['patent'].count().plot(kind='bar', rot=45)
+ticks = fig.get_xticks()
+labels = fig.get_xticklabels()
+
+p = 5  # Sparsity parameter
+l2 = [str(x.get_text()) for x in labels[::p]]
+fig.set_xticks(ticks[::p])
+fig.set_xticklabels(l2)
+plt.draw()
