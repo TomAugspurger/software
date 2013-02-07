51ea515466abee4e5550a3384fcb0d1c4c22869a
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-07 16:56:34 -0600

    Grouby year granted, not applied.

--- scripts.py
+++ scripts.py
@@ -7,13 +7,5 @@ s = pd.HDFStore('/Volumes/HDD/Users/tom/DataStorage/Patents/patents.h5')
 df = s['utility']
 
 ## Counts by year
-gr = df.groupby(df['appyear'])
-fig = gr['patent'].count().plot(kind='bar', rot=45)
-ticks = fig.get_xticks()
-labels = fig.get_xticklabels()
-
-p = 5  # Sparsity parameter
-l2 = [str(x.get_text()) for x in labels[::p]]
-fig.set_xticks(ticks[::p])
-fig.set_xticklabels(l2)
-plt.draw()
+gr = df.groupby(df['gyear'])
+fig = gr['patent'].count().plot(rot=45)
