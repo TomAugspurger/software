f587fb3a47376b7e2198644678365cb94e8dbb20
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-12 14:04:10 -0600

    Labels on plots.

--- scripts.py
+++ scripts.py
@@ -9,19 +9,27 @@ df = s['utility']
 ## Counts by year
 gr = df.groupby(df['gyear'])
 fig = gr['patent'].count().plot(rot=45)
+fig.set_xlabel('Year')
+fig.set_ylabel('Patents')
 
 ## Claims per year?  Not really. lag.
 plt.figure()
-(gr['nclaims'].sum() / gr['patent'].count()).plot()
+fig2 = (gr['nclaims'].sum() / gr['patent'].count()).plot()
+fig2.set_xlabel('Year')
+fig2.set_ylabel('Claims per Patent?')
 
 ## Patents by Country
 by_ctry = df.groupby('country')['patent'].count()
 by_ctry.sort()
-fig = by_ctry[-10:].plot(kind='barh')
+fig3 = by_ctry[-10:].plot(kind='barh')
+fig3.set_ylabel('Patents')
+
 
 ## By Country and time.
 by_ctry_time = df.groupby(['country', 'gyear'])['patent'].count()
 idx = by_ctry[-10:].index
 by_ctry_time = by_ctry_time.ix[idx]
 by_ctry_time.index = by_ctry_time.index.swaplevel(1, 0)
-(by_ctry_time.unstack()).plot()
+fig4 = (by_ctry_time.unstack()).plot()
+fig.set_xlabel('Year')
+fig.set_ylabel('Patents')
