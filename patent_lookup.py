0af0f675910c27c04cc8ff73a98834a3b43ea32e
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-17 08:33:45 -0600

    Cleanup.

--- patent_lookup.py
+++ patent_lookup.py
@@ -1,11 +1,9 @@
 import webbrowser
 
-base_url1 = r'http://patft.uspto.gov/netacgi/nph-Parser?TERM1='
-base_url2 = r'&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50'
-
-
 def lookup(num):
-    full = base_url1 + num + base_url2
+    base_url1 = r'http://patft.uspto.gov/netacgi/nph-Parser?TERM1='
+    base_url2 = r'&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50'
+    full = base_url1 + str(num) + base_url2
     webbrowser.open(full)
 
 if __name__ == '__main__':
