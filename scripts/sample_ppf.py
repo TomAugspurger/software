ae5671462ef4215e1a21f67d958405085f36fa4c
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-21 10:24:16 -0600

    ENH: Added initial view.  Removed other method.

--- scripts/sample_ppf.py
+++ scripts/sample_ppf.py
@@ -2,21 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-# TODO: Program in the viewing angle.
 # Sphere at origin: x^2 + y^2 + z*2 = r*2
 
 x = np.linspace(0, 2, 100)
 y = np.linspace(0, 2, 100)
 
-x_ = np.outer(np.cos(x), np.sin(y))
-y_ = np.outer(np.sin(x), np.sin(y))
-z_ = np.outer(np.ones(np.size(x)), np.cos(y))
-
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.plot_wireframe(x_, y_, z_)
-
-# Method 2
 X, Y = np.meshgrid(x, y)
 Z = np.sqrt(4 - X ** 2 - Y ** 2)
 Z[np.isnan(Z)] = 0
@@ -25,22 +15,20 @@ Z[np.isnan(Z)] = 0
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(1, 1, 1, projection='3d')
 
-ax1.set_xlabel('Capital Stock')
-ax1.set_ylabel('Investmnet')
-ax1.set_zlabel('Labor')
-ax1.set_title('Example PPF')
-p = ax1.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
-# Adjust view
-plt.savefig('../resources/example_ppf_wire.png', dpi=100)  # Up dpi for final
 
 # Surface
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(1, 1, 1, projection='3d')
 
-ax2.set_title('Example PPF')
-ax2.set_xlabel('Capital Stock')
-ax2.set_ylabel('Investmnet')
-ax2.set_zlabel('Labor')
+for ax in [ax1, ax2]:
+    ax.set_xlabel('Capital Stock')
+    ax.set_ylabel('Investmnet')
+    ax.set_zlabel('Labor')
+    ax.set_title('Example PPF')
+    ax.view_init(elev=14, azim=10)
+
+p = ax1.plot_wireframe(X, Y, Z, rstride=2, cstride=2)
+plt.savefig('../resources/example_ppf_wire.png', dpi=100)  # Up dpi for final
+
 p2 = ax2.plot_surface(X, Y, Z, cmap=plt.cm.jet)
-# Adjust view
 plt.savefig('../resources/example_ppf_surface.png', dpi=100)
