e616feb007e777e174d24714e7c6aed6abc13c1b
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-02-24 16:44:49 -0600

    ENH: Using a generator to support lists of patents.

--- scripts/patent_lookup.py
+++ scripts/patent_lookup.py
@@ -5,7 +5,10 @@ from cPickle import load
 class Lookup(object):
     """
     """
-    def __init__(self):
+    def __init__(self, patents):
+        """Allow num to be a single patent or a list?
+        """
+        self.patents = patents
         self.d_patent_to_int = None
         self.d_int_to_patent = None
 
@@ -22,13 +25,24 @@ class Lookup(object):
         except IOError:
             raise IOError('Can\'t get the dict.')
 
-    def patent_to_web(self, num):
+    def patent_to_web(self):
         """Go to USPTO website.
+        Use a generator to avoid open a bunch at once.
+        Either use g = self.patent_to_web() and g.next() or loop through
+        with an iterator (for i in g).
         """
-        base_url1 = r'http://patft.uspto.gov/netacgi/nph-Parser?TERM1='
-        base_url2 = r'&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50'
-        full = base_url1 + str(num) + base_url2
-        webbrowser.open(full)
+        def _patent_to_web(xs):
+            for i in xs:
+                print i
+                base_url1 = r'http://patft.uspto.gov/netacgi/nph-Parser?TERM1='
+                base_url2 = r'&Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=0&f=S&l=50'
+                full = base_url1 + str(i) + base_url2
+                yield webbrowser.open(full)
+
+        if isinstance(self.patents, int):
+            return _patent_to_web([self.patents])
+        elif isinstance(self.patents, list):
+            return _patent_to_web(self.patents)
 
     def int_to_web(self, int_):
         self.patent_to_web(self.int_to_patent(int_))
@@ -45,6 +59,6 @@ class Lookup(object):
 if __name__ == '__main__':
     # Quick access to website.
     import sys
-    num = sys.argv[1]
-    c = Lookup(d1=None, d2=None)
-    c.patent_to_web(num)
+    num = int(sys.argv[1])
+    c = Lookup(num)
+    g = c.patent_to_web().next()
