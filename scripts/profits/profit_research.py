bafd062728051e80ae44ea478b97ee9fa1aa0ccf
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-03-14 08:28:39 -0500

    Moving to quarterly.

--- scripts/profits/profit_research.py
+++ scripts/profits/profit_research.py
@@ -3,18 +3,18 @@
 # http://www.nber.org/~jbessen/dynass.dat.zip
 
 import pandas as pd
-
+import statsmodels.api as sm
 df = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/dynass.dat', sep='\t', index_col='pdpass')
 df2 = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/pdpcohdr.dat', sep='\t')
 
 # gvkeys for the lookup on WRDS compustat.
-with open('/Volumes/HDD/Users/tom/DataStorage/Patents/gvkeys.txt', 'w') as f:
-    f.write(df2[['gvkey']].to_string(index=False))
+# with open('/Volumes/HDD/Users/tom/DataStorage/Patents/gvkeys.txt', 'w') as f:
+#     f.write(df2[['gvkey']].to_string(index=False))
 
 # Unique key formed by company, date
 wrds = pd.read_csv(
     '/Volumes/HDD/Users/tom/DataStorage/Patents/profit_research.csv',
-    parse_dates=['datadate'], index_col=['gvkey', 'datadate'])
+    index_col=['gvkey', 'datadate'])
 
 """
 wrds.columns
@@ -38,5 +38,17 @@ COSTAT:
 NAICS: Classifier.
 """
 # Only NaNs are in XRD
-s = wrds[['GP', 'XRD', 'NAICS']].dropna()
+df1 = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/research_profit_q_1.csv',
+                  index_col=['gvkey', 'datadate'], parse_dates=['datadate'])
+df2 = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/research_profit_q_2.csv',
+                  index_col=['gvkey', 'datadate'], parse_dates=['datadate'])
+df = pd.concat([df1, df2])
+df = df.sort()
+df.columns = map(lambda x: x.lower(), df.columns)
 
+y = df.saleq - df.cogsq
+x = df[['xrdq']]
+x = sm.add_constant(x)
+
+suby = y.ix[:100]
+subx = x.ix[:100]
