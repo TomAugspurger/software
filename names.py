99fd88d8db96067cd828d90a6b2da35ece438e82
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-03-26 23:17:57 -0500

    Names to HDFStore.

--- names.py
+++ names.py
@@ -6,3 +6,11 @@ df2 = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/pdpcohdr.dat', sep
 # gvkeys for the lookup on WRDS compustat.
 with open('/Volumes/HDD/Users/tom/DataStorage/Patents/gvkeys.txt', 'w') as f:
     f.write(df2[['gvkey']].to_string(index=False))
+
+# For the HDFStore
+# gvkey is unique for this one.
+df2 = pd.read_csv('/Volumes/HDD/Users/tom/DataStorage/Patents/pdpcohdr.dat',
+                  sep='\t', index_col='gvkey')
+
+s = pd.HDFStore('/Volumes/HDD/Users/tom/DataStorage/Patents/patents.h5')
+s.append('names', df2)
